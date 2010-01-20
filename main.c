cd9c67e598e5d08303620c376656660a54f4b628
Matt O'Gorman <mog@rldn.net>
2010-01-19 23:31:05 -0600

    moving towards better control and understanding of the license

--- main.c
+++ main.c
@@ -1,37 +1,3 @@
-
-/******************************************************************************
-
-  Filename:		WiShield.cpp
-  Description:	WiShield library file for the WiShield 1.0
-
- ******************************************************************************
-
-  TCP/IP stack and driver for the WiShield 1.0 wireless devices
-
-  Copyright(c) 2009 Async Labs Inc. All rights reserved.
-
-  This program is free software; you can redistribute it and/or modify it
-  under the terms of version 2 of the GNU General Public License as
-  published by the Free Software Foundation.
-
-  This program is distributed in the hope that it will be useful, but WITHOUT
-  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-  more details.
-
-  You should have received a copy of the GNU General Public License along with
-  this program; if not, write to the Free Software Foundation, Inc., 59
-  Temple Place - Suite 330, Boston, MA  02111-1307, USA.
-
-  Contact Information:
-  <asynclabs@asynclabs.com>
-
-   Author               Date        Comment
-  ---------------------------------------------------------------
-   AsyncLabs			05/01/2009	Initial version
-   AsyncLabs			05/29/2009	Adding support for new library
-
- *****************************************************************************/
 #include <inttypes.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
